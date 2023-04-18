95c793e749634205ab09d73d84733f10056fedc7
Tiago zay <tiagozay@gmail.com>
2023-04-18 18:10:24 -0300

    Criando PrecificacaoContext
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

---
+++ src/contexts/PrecificacaoContext.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Precificacao } from '../models/Precificacao';
+import { useState } from 'react';
+import { createContext, ReactNode } from 'react';
+
+interface TypePrecificacaoContext 
+{
+    precificacoes?: Precificacao[]
+}
+
+export const PrecificacaoContext = createContext<TypePrecificacaoContext>({});
+
+export default function PrecificacoesProvider({children}: {children: ReactNode}) {
+    const [precificacoes, setPrecificacoes] = useState([
+        new Precificacao(1, 'Carro', 20, 250, true, 20),
+        new Precificacao(2, 'Moto', 10, 150, true, 25),
+        new Precificacao(3, 'Jegue', 10, 150, false, 10),
+        new Precificacao(4, 'Caminhão', 50, 550, true, 5),
+        new Precificacao(5, 'Trator', 50, 550, true, 5),
+        new Precificacao(6, 'Avião', 100, 1500, false, 2),
+    ]);
+
+    return (
+        <PrecificacaoContext.Provider value={{precificacoes}}>
+            {children}
+        </PrecificacaoContext.Provider>
+    )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PaginaBase from './pages/PaginaBase';
 import Home from './pages/Home';
 import Estacionamento from './pages/Estacionamento';
@@ -16,32 +16,34 @@ import CadastrarFormaDePagamento from './pages/CadastrarFormaDePagamento';
 import Usuarios from './pages/Usuarios';
 import CadastrarUsuario from './pages/CadastrarUsuario';
 import ConfiguracoesDoSistema from './pages/ConfiguracoesDoSistema';
+import PrecificacoesProvider from './contexts/PrecificacaoContext';
 
 
 function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<PaginaBase />}>
-          <Route index element={<Home />} />
-          <Route path='estacionamento' element={<Estacionamento />} />
-          <Route path='estacionamento/cadastrarTiket' element={<CadastrarTiket />} />
-          <Route path='mensalistas' element={<Mensalistas />} />
-          <Route path='mensalistas/cadastrarMensalista' element={<CadastrarMensalista />} />
-          <Route path='mensalidades' element={<Mensalidades />} />
-          <Route path='mensalidades/cadastrarMensalidade' element={<CadastrarMensalidade />} />
-          <Route path='precificacoes' element={<Precificacoes />} />
-          <Route path='precificacoes/cadastrarPrecificacao' element={<CadastrarPrecificacao />} />
-          <Route path='formasDePagamento' element={<FormasDePagamento />} />
-          <Route path='formasDePagamento/cadastrarFormaDePagamento' element={<CadastrarFormaDePagamento />} />
-          <Route path='usuarios' element={<Usuarios />} />
-          <Route path='usuarios/cadastrarUsuario' element={<CadastrarUsuario />} />
-          <Route path='configuracoes' element={<ConfiguracoesDoSistema />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-      
- 
+    <PrecificacoesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<PaginaBase />}>
+            <Route index element={<Home />} />
+            <Route path='estacionamento' element={<Estacionamento />} />
+            <Route path='estacionamento/cadastrarTiket' element={<CadastrarTiket />} />
+            <Route path='mensalistas' element={<Mensalistas />} />
+            <Route path='mensalistas/cadastrarMensalista' element={<CadastrarMensalista />} />
+            <Route path='mensalidades' element={<Mensalidades />} />
+            <Route path='mensalidades/cadastrarMensalidade' element={<CadastrarMensalidade />} />
+            <Route path='precificacoes' element={<Precificacoes />} />
+            <Route path='precificacoes/cadastrarPrecificacao' element={<CadastrarPrecificacao />} />
+            <Route path='formasDePagamento' element={<FormasDePagamento />} />
+            <Route path='formasDePagamento/cadastrarFormaDePagamento' element={<CadastrarFormaDePagamento />} />
+            <Route path='usuarios' element={<Usuarios />} />
+            <Route path='usuarios/cadastrarUsuario' element={<CadastrarUsuario />} />
+            <Route path='configuracoes' element={<ConfiguracoesDoSistema />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </PrecificacoesProvider>
+
   );
 }
 
