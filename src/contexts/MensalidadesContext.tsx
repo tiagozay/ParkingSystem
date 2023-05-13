3b1081f2e6063eddcca4a885dd7a09ba89b12212
Tiago zay <tiagozay@gmail.com>
2023-05-13 11:06:40 -0300

    Criando context de Mensalidade
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

---
+++ src/contexts/MensalidadesContext.tsx
@@ -0,0 +1,109 @@
+import React, {useContext} from 'react'
+import { useState } from 'react';
+import { createContext, ReactNode } from 'react';
+import { Mensalidade } from '../models/Mensalidade';
+import { useMensalistaContext } from './MensalistasContext';
+import { usePrecificacaoContext } from './PrecificacaoContext';
+import { useFormaDePagamentoContext } from './FormaDePagamentoContext';
+import { Mensalista } from '../models/Mensalista';
+import { Precificacao } from '../models/Precificacao';
+import { FormaDePagamento } from '../models/FormaDePagamento';
+
+interface TypeMensalidadeContext 
+{
+    mensalidades: Mensalidade[],
+    setMensalidades: Function
+}
+
+export const MensalidadeContext = createContext<TypeMensalidadeContext>({mensalidades: [], setMensalidades: () => {}});
+
+export default function MensalidadesProvider({children}: {children: ReactNode}) {
+    const {buscarMensalistaPorId} = useMensalistaContext();
+    const {buscaPrecificacaoPorId} = usePrecificacaoContext();
+    const {buscarFormaDePagamentoPorId} = useFormaDePagamentoContext();
+
+    const [mensalidades, setMensalidades] = useState([
+        new Mensalidade(
+            1,
+            buscarMensalistaPorId(1) as Mensalista,
+            buscaPrecificacaoPorId(1) as Precificacao,
+            buscarFormaDePagamentoPorId(1) as FormaDePagamento,
+            new Date(),
+        ),
+        new Mensalidade(
+            2,
+            buscarMensalistaPorId(2) as Mensalista,
+            buscaPrecificacaoPorId(2) as Precificacao,
+            buscarFormaDePagamentoPorId(1) as FormaDePagamento,
+            new Date(),
+        ),
+        new Mensalidade(
+            3,
+            buscarMensalistaPorId(3) as Mensalista,
+            buscaPrecificacaoPorId(2) as Precificacao,
+            buscarFormaDePagamentoPorId(2) as FormaDePagamento,
+            new Date(),
+        ),
+        new Mensalidade(
+            4,
+            buscarMensalistaPorId(4) as Mensalista,
+            buscaPrecificacaoPorId(1) as Precificacao,
+            buscarFormaDePagamentoPorId(3) as FormaDePagamento,
+            new Date(),
+        ),
+        new Mensalidade(
+            5,
+            buscarMensalistaPorId(1) as Mensalista,
+            buscaPrecificacaoPorId(1) as Precificacao,
+            buscarFormaDePagamentoPorId(3) as FormaDePagamento,
+            new Date(' 04 12 2023 '),
+        ),
+        new Mensalidade(
+            6,
+            buscarMensalistaPorId(2) as Mensalista,
+            buscaPrecificacaoPorId(1) as Precificacao,
+            buscarFormaDePagamentoPorId(3) as FormaDePagamento,
+            new Date(' 04 05 2023 '),
+        ),
+    ]);
+
+    return (
+        <MensalidadeContext.Provider value={{mensalidades, setMensalidades}}>
+            {children}
+        </MensalidadeContext.Provider>
+    );
+}
+
+export const useMensalidadeContext = () => {
+    const {mensalidades, setMensalidades} = useContext(MensalidadeContext);
+
+    function buscarMensalidadePorId(id: number)
+    {
+        return mensalidades.find( mensalidade => mensalidade.id === id );
+    }
+
+    function adicionarMensalidade(mensalidade: Mensalidade)
+    {
+        //Gera provisióriamente um id em sequência do ultimo registro, para simular o que um banco de dados faria
+        const ultimaMensalidadeCadastrada = mensalidades[mensalidades.length - 1];
+        if(ultimaMensalidadeCadastrada){
+            mensalidade.id = (ultimaMensalidadeCadastrada.id as number) + 1;
+        }else{
+            mensalidade.id = 1;
+        }
+
+        setMensalidades([...mensalidades, mensalidade] );
+    }
+
+    function removerMensalidade(id: number)
+    {
+        setMensalidades( mensalidades.filter( mensalidade => mensalidade.id !== id ) );
+    }
+
+    return {
+        mensalidades,
+        buscarMensalidadePorId,
+        adicionarMensalidade,
+        removerMensalidade
+    }
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -23,6 +23,7 @@ import FormasDePagamentoProvider from './contexts/FormaDePagamentoContext';
 import VisualizarTiket from './pages/VisualizarTiket';
 import MensalistasProvider from './contexts/MensalistasContext';
 import EditarMensalista from './pages/EditarMensalista';
+import MensalidadesProvider from './contexts/MensalidadesContext';
 
 
 function AppRoutes() {
@@ -30,31 +31,33 @@ function AppRoutes() {
     <PrecificacoesProvider>
       <FormasDePagamentoProvider>
         <MensalistasProvider>
-          <TiketsProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route path='/' element={<PaginaBase />}>
-                  <Route index element={<Home />} />
-                  <Route path='estacionamento' element={<Estacionamento />} />
-                  <Route path='estacionamento/cadastrarTiket' element={<CadastrarTiket />} />
-                  <Route path='estacionamento/editarTiket/:id' element={<EditarTiket />} />
-                  <Route path='estacionamento/visualizarTiket/:id' element={<VisualizarTiket />} />
-                  <Route path='mensalistas' element={<Mensalistas />} />
-                  <Route path='mensalistas/cadastrarMensalista' element={<CadastrarMensalista />} />
-                  <Route path='mensalistas/editarMensalista/:id' element={<EditarMensalista />} />
-                  <Route path='mensalidades' element={<Mensalidades />} />
-                  <Route path='mensalidades/cadastrarMensalidade' element={<CadastrarMensalidade />} />
-                  <Route path='precificacoes' element={<Precificacoes />} />
-                  <Route path='precificacoes/cadastrarPrecificacao' element={<CadastrarPrecificacao />} />
-                  <Route path='formasDePagamento' element={<FormasDePagamento />} />
-                  <Route path='formasDePagamento/cadastrarFormaDePagamento' element={<CadastrarFormaDePagamento />} />
-                  <Route path='usuarios' element={<Usuarios />} />
-                  <Route path='usuarios/cadastrarUsuario' element={<CadastrarUsuario />} />
-                  <Route path='configuracoes' element={<ConfiguracoesDoSistema />} />
-                </Route>
-              </Routes>
-            </BrowserRouter>
-          </TiketsProvider>
+          <MensalidadesProvider>
+            <TiketsProvider>
+              <BrowserRouter>
+                <Routes>
+                  <Route path='/' element={<PaginaBase />}>
+                    <Route index element={<Home />} />
+                    <Route path='estacionamento' element={<Estacionamento />} />
+                    <Route path='estacionamento/cadastrarTiket' element={<CadastrarTiket />} />
+                    <Route path='estacionamento/editarTiket/:id' element={<EditarTiket />} />
+                    <Route path='estacionamento/visualizarTiket/:id' element={<VisualizarTiket />} />
+                    <Route path='mensalistas' element={<Mensalistas />} />
+                    <Route path='mensalistas/cadastrarMensalista' element={<CadastrarMensalista />} />
+                    <Route path='mensalistas/editarMensalista/:id' element={<EditarMensalista />} />
+                    <Route path='mensalidades' element={<Mensalidades />} />
+                    <Route path='mensalidades/cadastrarMensalidade' element={<CadastrarMensalidade />} />
+                    <Route path='precificacoes' element={<Precificacoes />} />
+                    <Route path='precificacoes/cadastrarPrecificacao' element={<CadastrarPrecificacao />} />
+                    <Route path='formasDePagamento' element={<FormasDePagamento />} />
+                    <Route path='formasDePagamento/cadastrarFormaDePagamento' element={<CadastrarFormaDePagamento />} />
+                    <Route path='usuarios' element={<Usuarios />} />
+                    <Route path='usuarios/cadastrarUsuario' element={<CadastrarUsuario />} />
+                    <Route path='configuracoes' element={<ConfiguracoesDoSistema />} />
+                  </Route>
+                </Routes>
+              </BrowserRouter>
+            </TiketsProvider>
+          </MensalidadesProvider>
         </MensalistasProvider>
       </FormasDePagamentoProvider>
     </PrecificacoesProvider>
