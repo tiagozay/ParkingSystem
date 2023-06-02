65266216478ec4d033575d5326cb7dcc903a9928
Tiago zay <tiagozay@gmail.com>
2023-06-02 16:50:22 -0300

    Instalando doctrine e ignorando pasta vendor do composer
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- back-end/vendor/composer/autoload_real.php
+++ back-end/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit1977c354fc2fe535f17ae60d0215f7bc
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit1977c354fc2fe535f17ae60d0215f7bc', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit1977c354fc2fe535f17ae60d0215f7bc', 'loadClassLoader'));
@@ -50,6 +52,24 @@ class ComposerAutoloaderInit1977c354fc2fe535f17ae60d0215f7bc
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit1977c354fc2fe535f17ae60d0215f7bc::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire1977c354fc2fe535f17ae60d0215f7bc($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire1977c354fc2fe535f17ae60d0215f7bc($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
