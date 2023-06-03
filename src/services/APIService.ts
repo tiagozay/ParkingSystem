4b98c8e289da107419a8c735b0d3622244708438
Tiago zay <tiagozay@gmail.com>
2023-06-03 15:02:55 -0300

    Criando APIService
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

---
+++ src/services/APIService.ts
@@ -0,0 +1,22 @@
+export abstract class APIService 
+{
+    private static url: string = "http://localhost:8080/";
+
+    public static enviaObjeto(arquivo: string, objeto: any)
+    {
+        return fetch( `${this.url}${arquivo}` , {
+            method: "POST",
+            body: JSON.stringify(objeto),
+        })
+            .then(res => {
+                if(!res.ok){
+                    throw new Error("Erro na requisição, code: "+res.status);
+                }
+                return res.json();
+            })
+            .catch( error => {
+                console.error(error);
+                throw new Error("Erro na requisição");
+            } )
+    }
+}
