a715d01a6d1c117cf157adc7a85a086561b2814d
Tiago zay <tiagozay@gmail.com>
2023-06-03 09:54:17 -0300

    Mapeando FormaDePagamento
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- back-end/src/Domain/Model/FormaDePagamento.php
+++ back-end/src/Domain/Model/FormaDePagamento.php
@@ -1,14 +1,28 @@
 <?php
     namespace ParkSistem\Domain\Model;
 
+    use Doctrine\ORM\Mapping\Column;
+    use Doctrine\ORM\Mapping\Entity;
+    use Doctrine\ORM\Mapping\GeneratedValue;
+    use Doctrine\ORM\Mapping\Id;
     use DomainException;
 
+    #[Entity]
     class FormaDePagamento
     {
+        #[Id]
+        #[GeneratedValue]
+        #[Column()]
         private ?int $id;
-        private $nomeFormaDePagamento;
-        private $ativa;
-        private $descontinuada;
+
+        #[Column(length: 50)]
+        private string $nomeFormaDePagamento;
+
+        #[Column()]
+        private bool $ativa;
+
+        #[Column()]
+        private bool $descontinuada;
 
         /**
          * @throws DomainException
