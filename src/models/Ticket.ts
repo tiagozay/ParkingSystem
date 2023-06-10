be88bb75bcf3e65e5e73cfec80866645c65cafe2
Tiago zay <tiagozay@gmail.com>
2023-06-10 11:55:50 -0300

    ... propriedade "mensalidade" para tiket e validações no front-end
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -4,8 +4,7 @@ import { Mensalista } from "./Mensalista.js";
 import { Precificacao } from "./Precificacao.js";
 import { Mensalidade } from "./Mensalidade.js";
 
-export class Ticket
-{
+export class Ticket {
     public id: number | null = null;
     public placaVeiculo: string;
     public marcaVeiculo: string;
@@ -27,12 +26,12 @@ export class Ticket
         dataEntrada: Date,
         dataSaida: Date | null,
         precificacao: Precificacao,
-        status: "Em aberto" | "Pago", 
+        status: "Em aberto" | "Pago",
         numeroDaVaga: string | null,
         formaDePagamento: FormaDePagamento | null = null,
         mensalista: Mensalista | null = null,
         mensalidade: Mensalidade | null = null
-    ){
+    ) {
         this.id = id;
         this.placaVeiculo = placaVeiculo;
         this.marcaVeiculo = marcaVeiculo;
@@ -56,19 +55,19 @@ export class Ticket
         formaDePagamento: FormaDePagamento | null = null,
         mensalista: Mensalista | null = null,
         mensalidade: Mensalidade | null = null
-    ){
+    ) {
         this.placaVeiculo = placaVeiculo;
         this.marcaVeiculo = marcaVeiculo;
         this.modeloVeiculo = modeloVeiculo;
         this.precificacao = precificacao;
         this.mensalista = mensalista;
-        
+
         console.log("aaaaaa");
         this.mensalidade = mensalidade;
         console.log("bbbbbb");
 
         //Se o ticket ainda não foi pago e a data de saída e a forma de pagamento foi recebida nessa função de editar, é sinal que o operador realizou o pagamento deste ticket, aí defino a data de saída e a forma de pagamento e mudo seu status para "Pago"
-        if(this.status === "Em aberto" && dataSaida && formaDePagamento){
+        if (this.status === "Em aberto" && dataSaida && formaDePagamento) {
             this._dataDeSaida = dataSaida;
             this.formaDePagamento = formaDePagamento;
             this.status = "Pago";
@@ -76,8 +75,7 @@ export class Ticket
 
     }
 
-    set precificacao(precificacao: Precificacao)
-    {
+    set precificacao(precificacao: Precificacao) {
         const ehNovoTiket = !this.id;
 
         //Indica se este setter não foi chamado a partir do construtor, e sim da função editar, que é chamada quando a propriedade já foi incializada (quando ela não é undefined)
@@ -85,25 +83,24 @@ export class Ticket
 
         const ehPrecificacaoDiferenteDaJaCadastrada = this._precificacao?.id !== precificacao?.id;
 
-        if(
-            (ehNovoTiket) || 
+        if (
+            (ehNovoTiket) ||
             (ehChamadaDeEdicao && ehPrecificacaoDiferenteDaJaCadastrada)
-        ){
-            if(!precificacao.ativa || precificacao.descontinuada){
+        ) {
+            if (!precificacao.ativa || precificacao.descontinuada) {
                 throw new Error("Precificação inválida (inativa ou descontinuada)");
             }
-        }   
+        }
 
         this._precificacao = precificacao;
     }
 
-    set formaDePagamento(formaDePagamento: FormaDePagamento | null)
-    {
+    set formaDePagamento(formaDePagamento: FormaDePagamento | null) {
         const ehNovoTiket = !this.id;
 
-        if(formaDePagamento){
-            if(ehNovoTiket || this.status === "Em aberto"){
-                if(!formaDePagamento.ativa || formaDePagamento.descontinuada){
+        if (formaDePagamento) {
+            if (ehNovoTiket || this.status === "Em aberto") {
+                if (!formaDePagamento.ativa || formaDePagamento.descontinuada) {
                     throw new Error("Forma de pagamento inválida (inativa ou descontinuada)");
                 }
             }
@@ -112,8 +109,7 @@ export class Ticket
         this._formaDePagamento = formaDePagamento;
     }
 
-    set mensalista(mensalista: Mensalista | null)
-    {
+    set mensalista(mensalista: Mensalista | null) {
         const ehNovoTiket = !this.id;
 
         //Indica se este setter não foi chamado a partir do construtor, e sim da função editar, que é chamada quando a propriedade já foi incializada (quando ela não é undefined)
@@ -122,25 +118,24 @@ export class Ticket
         const ehMensalistaDiferenteDoJaCadastrado = this._mensalista?.id !== mensalista?.id;
 
         //Só é realizada a verificação se for um novo tiket ou se estiver editando e for passado um mensalista diferente do já cadastrado
-        if(mensalista){
-            if(
+        if (mensalista) {
+            if (
                 (ehNovoTiket) ||
-                ( ehChamadaDeEdicao && ehMensalistaDiferenteDoJaCadastrado)
-            ){
-                if(!mensalista.ativo || mensalista.descontinuado){
+                (ehChamadaDeEdicao && ehMensalistaDiferenteDoJaCadastrado)
+            ) {
+                if (!mensalista.ativo || mensalista.descontinuado) {
                     throw new Error("Mensalista inválido (inativo ou descontinuado)");
                 }
             }
         }
-        
+
         this._mensalista = mensalista;
     }
 
-    set mensalidade(mensalidade: Mensalidade | null)
-    {
-        if(this.mensalista && !mensalidade){
+    set mensalidade(mensalidade: Mensalidade | null) {
+        if (this.mensalista && !mensalidade) {
             throw new Error("Informe a mensalidade corretamente!");
-        }  
+        }
 
         const ehNovoTiket = !this.id;
 
@@ -149,12 +144,12 @@ export class Ticket
 
         const ehMensalidadeDiferenteDaJaCadastrada = this._mensalidade?.id !== mensalidade?.id;
 
-        if(
+        if (
             (ehNovoTiket) ||
             (ehChamadaDeEdicao && ehMensalidadeDiferenteDaJaCadastrada)
-        ){
-            
-            if(mensalidade?.status === 'Vencida' || mensalidade?.descontinuada){
+        ) {
+
+            if (mensalidade?.status === 'Vencida' || mensalidade?.descontinuada) {
                 throw new Error("Mensalidade inválida (vencida ou descontinuada)");
             }
 
@@ -163,28 +158,23 @@ export class Ticket
         this._mensalidade = mensalidade;
     }
 
-    get precificacao(): Precificacao
-    {
+    get precificacao(): Precificacao {
         return this._precificacao;
     }
 
-    get formaDePagamento(): FormaDePagamento | null
-    {
+    get formaDePagamento(): FormaDePagamento | null {
         return this._formaDePagamento;
     }
 
-    get dataDeSaida(): Date | null
-    {
+    get dataDeSaida(): Date | null {
         return this._dataDeSaida;
     }
 
-    get mensalista()
-    {
+    get mensalista() {
         return this._mensalista;
-    } 
+    }
 
-    calculaTotalAPagar(valorPorHora: number): number
-    {
+    calculaTotalAPagar(valorPorHora: number): number {
         const tempoDecorrido = this.tempoDecorrido;
 
         const tempoDividido = tempoDecorrido.split(":");
@@ -196,12 +186,11 @@ export class Ticket
     }
 
 
-    get tempoDecorrido(): string
-    {
+    get tempoDecorrido(): string {
 
         let dataParaOCalculo = new Date();
 
-        if(this._dataDeSaida){
+        if (this._dataDeSaida) {
             dataParaOCalculo = this._dataDeSaida;
         }
 
@@ -213,8 +202,8 @@ export class Ticket
         let minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
 
         //Formata a data para que numeros de apenas 1 digito tenham 0 antes, ex 1 vira 01
-        let horaFormatada = String(horas).length == 1 ? "0"+horas : String(horas);
-        let minutosFormatados = String(minutos).length == 1 ? "0"+minutos : String(minutos);
+        let horaFormatada = String(horas).length == 1 ? "0" + horas : String(horas);
+        let minutosFormatados = String(minutos).length == 1 ? "0" + minutos : String(minutos);
         return `${horaFormatada}:${minutosFormatados}`;
     }
 }
