1d61c608e72b03fdc4270d024ce1f159d0c8a114
Tiago zay <tiagozay@gmail.com>
2023-06-17 15:16:23 -0300

    Criando os relatórios da Home
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,12 +3,33 @@ import './Home.css';
 import BoasVindas from '../../components/BoasVindas';
 import IconeEstacionamento from './proibido-estacionar.svg';
 import { usePrecificacaoContext } from '../../contexts/PrecificacaoContext';
+import RelatorioService from '../../services/RelatorioServcie';
+import { useMensalistaContext } from '../../contexts/MensalistasContext';
+import { useTicketContext } from '../../contexts/TicketContext';
+import { useMensalidadeContext } from '../../contexts/MensalidadesContext';
 
 export default function Home() {
 
+  const { mensalistas } = useMensalistaContext();
+  const { tickets } = useTicketContext();
+  const { mensalidades } = useMensalidadeContext();
   const { precificacoes } = usePrecificacaoContext();
 
   const precificacoesValidas = precificacoes.filter(precificacao => precificacao.ativa && !precificacao.descontinuada);
+
+  const totalDeMensalistas = RelatorioService.totalDeMensalistas(mensalistas);
+  const totalDeMensalistasAtivos = RelatorioService.totalDeMensalistasAtivos(mensalistas);
+  const totalDeMensalistasInativos = RelatorioService.totalDeMensalistasInativos(mensalistas);
+  const valorTotalFaturadoDeTicketsAvulsos = RelatorioService.valorTotalFaturadoDeTicketsAvulsos(tickets);
+  const totalTicketsEmAberto = RelatorioService.totalTicketsEmAberto(tickets);
+  const totalTicketsPagos = RelatorioService.totalTicketsPagos(tickets);
+  const valorTotalFaturadoDeMensalidades = RelatorioService.valorTotalFaturadoDeMensalidades(mensalidades);
+  const totalDeMensalidadesEmDia = RelatorioService.totalDeMensalidadesEmDia(mensalidades);
+  const totalDeMensalidadesVencidas = RelatorioService.totalDeMensalidadesVencidas(mensalidades);
+  const totalDeVagasEstacionamento = RelatorioService.totalDeVagasEstacionamento(precificacoes);
+  const totalDeVagasLivresEstacionamento = RelatorioService.totalDeVagasLivresEstacionamento(precificacoes, tickets);
+  const totalDeVagasOcupadasEstacionamento = RelatorioService.totalDeVagasOcupadasEstacionamento(precificacoes, tickets);
+
 
   return (
     <section id="home">
@@ -34,19 +55,19 @@ export default function Home() {
             <i className="material-icons">warehouse</i>
             <div className="tituloEValor">
               <p>Total de vagas</p>
-              <p className="valorCard">120</p>
+              <p className="valorCard">{totalDeVagasEstacionamento}</p>
             </div>
           </div>
 
           <div className="cardInformacoes">
             <div className="cardInformacoes__primeiraInformacao">
               <span className="tituloInfo">Livre</span>
-              <span className="valueInfo">117</span>
+              <span className="valueInfo">{totalDeVagasLivresEstacionamento}</span>
             </div>
             <i className="material-icons">sync_alt</i>
             <div className="cardInformacoes__segundaInformacao">
               <span className="tituloInfo">Ocupadas</span>
-              <span className="valueInfo">3</span>
+              <span className="valueInfo">{totalDeVagasOcupadasEstacionamento}</span>
             </div>
           </div>
 
@@ -57,19 +78,19 @@ export default function Home() {
             <i className="material-icons">payments</i>
             <div className="tituloEValor">
               <p>Mensais</p>
-              <p className="valorCard">R$ 12.900</p>
+              <p className="valorCard">{valorTotalFaturadoDeMensalidades.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
             </div>
           </div>
 
           <div className="cardInformacoes">
             <div className="cardInformacoes__primeiraInformacao">
               <span className="tituloInfo">Em dia</span>
-              <span className="valueInfo">1</span>
+              <span className="valueInfo">{totalDeMensalidadesEmDia}</span>
             </div>
             <i className="material-icons">sync_alt</i>
             <div className="cardInformacoes__segundaInformacao">
               <span className="tituloInfo">Vencidas</span>
-              <span className="valueInfo">2</span>
+              <span className="valueInfo">{totalDeMensalidadesVencidas}</span>
             </div>
           </div>
 
@@ -80,19 +101,19 @@ export default function Home() {
             <i className="material-icons">attach_money</i>
             <div className="tituloEValor">
               <p>Avulsos</p>
-              <p className="valorCard">R$ 67,233.65</p>
+              <p className="valorCard">{valorTotalFaturadoDeTicketsAvulsos.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
             </div>
           </div>
 
           <div className="cardInformacoes">
             <div className="cardInformacoes__primeiraInformacao">
-              <span className="tituloInfo">Pagas</span>
-              <span className="valueInfo">5</span>
+              <span className="tituloInfo">Pagos</span>
+              <span className="valueInfo">{totalTicketsPagos}</span>
             </div>
             <i className="material-icons">sync_alt</i>
             <div className="cardInformacoes__segundaInformacao">
-              <span className="tituloInfo">Abertas</span>
-              <span className="valueInfo">3</span>
+              <span className="tituloInfo">Abertos</span>
+              <span className="valueInfo">{totalTicketsEmAberto}</span>
             </div>
           </div>
 
@@ -103,19 +124,19 @@ export default function Home() {
             <i className="material-icons">groups</i>
             <div className="tituloEValor">
               <p>Mensalistas</p>
-              <p className="valorCard">120</p>
+              <p className="valorCard">{totalDeMensalistas}</p>
             </div>
           </div>
 
           <div className="cardInformacoes">
             <div className="cardInformacoes__primeiraInformacao">
               <span className="tituloInfo">Ativos</span>
-              <span className="valueInfo">3</span>
+              <span className="valueInfo">{totalDeMensalistasAtivos}</span>
             </div>
             <i className="material-icons">sync_alt</i>
             <div className="cardInformacoes__segundaInformacao">
               <span className="tituloInfo">Inativos</span>
-              <span className="valueInfo">0</span>
+              <span className="valueInfo">{totalDeMensalistasInativos}</span>
             </div>
           </div>
 
---
+++ src/services/RelatorioServcie.ts
@@ -0,0 +1,93 @@
+import { Mensalidade } from "../models/Mensalidade";
+import { Mensalista } from "../models/Mensalista";
+import { Precificacao } from "../models/Precificacao";
+import { Ticket } from "../models/Ticket";
+
+export default abstract class RelatorioService
+{
+    private static mensalistasNaoDescontinuados(mensalistas: Mensalista[]): Mensalista[]
+    {
+        return mensalistas.filter( mensalistas => !mensalistas.descontinuado );
+    }
+
+    private static precificacoesNaoDescontinuadas( precificacoes: Precificacao[] ): Precificacao[]
+    {
+        return precificacoes.filter( precificacao => !precificacao.descontinuada );
+    }
+
+    public static totalDeMensalistas(mensalistas: Mensalista[]): number
+    {
+        return this.mensalistasNaoDescontinuados(mensalistas).length;
+    }
+
+    public static totalDeMensalistasAtivos(mensalistas: Mensalista[]): number
+    {
+        return this.mensalistasNaoDescontinuados(mensalistas).filter( mensalista => mensalista.ativo ).length;
+    }
+
+    public static totalDeMensalistasInativos(mensalistas: Mensalista[]): number
+    {
+        return this.mensalistasNaoDescontinuados(mensalistas).filter( mensalista => !mensalista.ativo ).length;
+    }
+
+    public static valorTotalFaturadoDeTicketsAvulsos(tickets: Ticket[]): number
+    {
+        const ticketsPagosAvulsos = tickets.filter( ticket => ticket.status === "Pago" && !ticket.mensalista );
+
+        return ticketsPagosAvulsos.reduce( (previusValue, ticket) =>  ticket.calculaTotalAPagar(ticket.valorHora) + previusValue , 0 );
+    }
+
+    public static totalTicketsEmAberto(tickets: Ticket[]): number
+    {
+        return tickets.filter( ticket => ticket.status === "Em aberto" ).length;
+    }
+
+    public static totalTicketsPagos(tickets: Ticket[]): number
+    {
+        return tickets.filter( ticket => ticket.status === "Pago" ).length;
+    }
+
+    public static valorTotalFaturadoDeMensalidades(mensalidades: Mensalidade[]): number
+    {
+        return mensalidades.reduce( (previusValue, mensalidade) => mensalidade.valor + previusValue , 0 );
+    }
+
+    public static totalDeMensalidadesEmDia(mensalidades: Mensalidade[]): number
+    {
+        return mensalidades.filter( mensalidade => mensalidade.status === "Em dia" ).length;
+    }
+
+    public static totalDeMensalidadesVencidas(mensalidades: Mensalidade[]): number
+    {
+        return mensalidades.filter( mensalidade => mensalidade.status === "Vencida" ).length;
+    }
+
+    public static totalDeVagasEstacionamento(precificacoes: Precificacao[]): number
+    {
+        const precificacoesNaoDescontinuadas = this.precificacoesNaoDescontinuadas(precificacoes);
+
+        return precificacoesNaoDescontinuadas.reduce( (previusValue, precificacao) => precificacao.numeroDeVagas + previusValue, 0 );
+    }
+
+    public static calculaTotalDeVagasLivresDeDeterminadaCategoria(categoria: Precificacao, tickets: Ticket[])
+    {
+        const ticketsEmAbertoDeCategoria = tickets.filter( ticket => ticket.status === "Em aberto" && ticket.precificacao.id === categoria.id );
+
+        return categoria.numeroDeVagas - ticketsEmAbertoDeCategoria.length;
+    }
+
+    public static totalDeVagasLivresEstacionamento(precificacoes: Precificacao[], tickets: Ticket[]): number
+    {
+        const vagasLivresDeCadaCategoria = precificacoes.map( precificacao => this.calculaTotalDeVagasLivresDeDeterminadaCategoria(precificacao, tickets) );
+
+        return vagasLivresDeCadaCategoria.reduce( (previusValue, vagasLivres) => vagasLivres + previusValue, 0 );
+    }
+
+    public static totalDeVagasOcupadasEstacionamento(precificacoes: Precificacao[], tickets: Ticket[]): number
+    {
+        const totalDeVagasEstacionamento = this.totalDeVagasEstacionamento(precificacoes);
+        const vagasLivresEstacionamento  = this.totalDeVagasLivresEstacionamento(precificacoes, tickets);
+
+        return totalDeVagasEstacionamento - vagasLivresEstacionamento;
+    }
+}
