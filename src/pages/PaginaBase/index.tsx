d017e92cbcc52e78b59fe69e8494b00909cb559b
Tiago zay <tiagozay@gmail.com>
2023-06-24 16:58:57 -0300

    Jogando os providers dos contexts na página base
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/pages/PaginaBase/index.tsx
+++ src/pages/PaginaBase/index.tsx
@@ -1,10 +1,17 @@
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 import './PaginaBase.css';
 import Logo from './car.svg';
 import LoginService from '../../services/LoginService';
 import { useState } from 'react';
-import {ThreeDots} from "react-loader-spinner";
+import { ThreeDots } from "react-loader-spinner";
+import UsuariosProvider from '../../contexts/UsuariosContext';
+import SistemaProvider from '../../contexts/SistemaContext';
+import PrecificacoesProvider from '../../contexts/PrecificacaoContext';
+import FormasDePagamentoProvider from '../../contexts/FormaDePagamentoContext';
+import MensalistasProvider from '../../contexts/MensalistasContext';
+import MensalidadesProvider from '../../contexts/MensalidadesContext';
+import TicketsProvider from '../../contexts/TicketContext';
 
 export default function PaginaBase() {
 
@@ -12,115 +19,128 @@ export default function PaginaBase() {
 
     const navigate = useNavigate();
 
-    useEffect( () => {
+    useEffect(() => {
         LoginService.indicadorUsuarioLogado()
-        .then( indicador => {
+            .then(indicador => {
 
-            if(!indicador){
-                navigate("/login");
-                return;
-            }
+                if (!indicador) {
+                    navigate("/login");
+                    return;
+                }
 
-            setPermisaoParaRenderizar(indicador);
-        } )
-    }, [] );
-    
+                setPermisaoParaRenderizar(indicador);
+            })
+    }, []);
+
 
     return (
 
-        permisaoParaRenderizar ? 
+        permisaoParaRenderizar ?
 
-        <>
-            <section id="menu">
-                <div id="headerMenu">
-                    <a href="#" id="linkRecarregar">
-                        <img id="logoSistema" src={Logo} />
-                        <span id="nomeSistema">Estacionamento</span>
-                    </a>
-                </div>
+            <UsuariosProvider>
+                <SistemaProvider>
+                    <PrecificacoesProvider>
+                        <FormasDePagamentoProvider>
+                            <MensalistasProvider>
+                                <MensalidadesProvider>
+                                    <TicketsProvider>
+                                        <section id="menu">
+                                            <div id="headerMenu">
+                                                <a href="#" id="linkRecarregar">
+                                                    <img id="logoSistema" src={Logo} />
+                                                    <span id="nomeSistema">Estacionamento</span>
+                                                </a>
+                                            </div>
 
-                <div id="opcoesMenu">
-                    <div className="opcoesMenu__titulo">Menu</div>
-                    <NavLink to='/' className={ ({isActive}) => `
-                        opcaoMenu btnTrocarDePaginaMenu
-                        ${ isActive ? 'opcaoMenuSelecionado' : ''}
-                    ` } data-pagina="home">
-                        <i className="material-icons">home</i>
-                        Home
-                    </NavLink>
-                    <NavLink to='/estacionamento' className={ ({isActive}) => `
-                        opcaoMenu btnTrocarDePaginaMenu
-                        ${ isActive ? 'opcaoMenuSelecionado' : ''}
-                    ` } data-pagina="estacionamento">
-                        <i className="material-icons">local_parking</i>
-                        Estacionamento
-                    </NavLink>
-                    <NavLink to='/mensalistas' className={ ({isActive}) => `
-                        opcaoMenu btnTrocarDePaginaMenu
-                        ${ isActive ? 'opcaoMenuSelecionado' : ''}
-                    ` } data-pagina="mensalistas">
-                        <i className="material-icons">groups</i>
-                        Mensalistas
-                    </NavLink>
-                    <NavLink to='/mensalidades' className={ ({isActive}) => `
-                        opcaoMenu btnTrocarDePaginaMenu
-                        ${ isActive ? 'opcaoMenuSelecionado' : ''}
-                    ` } data-pagina="mensalidades">
-                        <i className="material-icons">payments</i>
-                        Mensalidades
-                    </NavLink>
-                    <div className="opcoesMenu__titulo">Administracao</div>
-                    <NavLink to='/precificacoes' className={ ({isActive}) => `
-                        opcaoMenu btnTrocarDePaginaMenu
-                        ${ isActive ? 'opcaoMenuSelecionado' : ''}
-                    ` } data-pagina="precificacoes">
-                        <i className="material-icons">attach_money</i>
-                        Precificações
-                    </NavLink>
-                    <NavLink to='/formasDePagamento' className={ ({isActive}) => `
-                        opcaoMenu btnTrocarDePaginaMenu
-                        ${ isActive ? 'opcaoMenuSelecionado' : ''}
-                    ` } data-pagina="formas_de_pagamento">
-                        <i className="material-icons">credit_card</i>
-                        Formas de pagamento
-                    </NavLink>
-                    <NavLink to='/usuarios' className={ ({isActive}) => `
-                        opcaoMenu btnTrocarDePaginaMenu
-                        ${ isActive ? 'opcaoMenuSelecionado' : ''}
-                    ` } data-pagina="usuarios">
-                        <i className="material-icons">group</i>
-                        Usuários
-                    </NavLink>
-                    <NavLink to='/configuracoes' className={ ({isActive}) => `
-                        opcaoMenu btnTrocarDePaginaMenu
-                        ${ isActive ? 'opcaoMenuSelecionado' : ''}
-                    ` } data-pagina="formularioConfigSistema">
-                        <i className="material-icons">settings</i>
-                        Sistema
-                    </NavLink>
-                </div>
+                                            <div id="opcoesMenu">
+                                                <div className="opcoesMenu__titulo">Menu</div>
+                                                <NavLink to='/' className={({ isActive }) => `
+                                                opcaoMenu btnTrocarDePaginaMenu
+                                                ${isActive ? 'opcaoMenuSelecionado' : ''}
+                                            ` } data-pagina="home">
+                                                    <i className="material-icons">home</i>
+                                                    Home
+                                                </NavLink>
+                                                <NavLink to='/estacionamento' className={({ isActive }) => `
+                                                opcaoMenu btnTrocarDePaginaMenu
+                                                ${isActive ? 'opcaoMenuSelecionado' : ''}
+                                            ` } data-pagina="estacionamento">
+                                                    <i className="material-icons">local_parking</i>
+                                                    Estacionamento
+                                                </NavLink>
+                                                <NavLink to='/mensalistas' className={({ isActive }) => `
+                                                opcaoMenu btnTrocarDePaginaMenu
+                                                ${isActive ? 'opcaoMenuSelecionado' : ''}
+                                            ` } data-pagina="mensalistas">
+                                                    <i className="material-icons">groups</i>
+                                                    Mensalistas
+                                                </NavLink>
+                                                <NavLink to='/mensalidades' className={({ isActive }) => `
+                                                opcaoMenu btnTrocarDePaginaMenu
+                                                ${isActive ? 'opcaoMenuSelecionado' : ''}
+                                            ` } data-pagina="mensalidades">
+                                                    <i className="material-icons">payments</i>
+                                                    Mensalidades
+                                                </NavLink>
+                                                <div className="opcoesMenu__titulo">Administracao</div>
+                                                <NavLink to='/precificacoes' className={({ isActive }) => `
+                                                opcaoMenu btnTrocarDePaginaMenu
+                                                ${isActive ? 'opcaoMenuSelecionado' : ''}
+                                            ` } data-pagina="precificacoes">
+                                                    <i className="material-icons">attach_money</i>
+                                                    Precificações
+                                                </NavLink>
+                                                <NavLink to='/formasDePagamento' className={({ isActive }) => `
+                                                opcaoMenu btnTrocarDePaginaMenu
+                                                ${isActive ? 'opcaoMenuSelecionado' : ''}
+                                            ` } data-pagina="formas_de_pagamento">
+                                                    <i className="material-icons">credit_card</i>
+                                                    Formas de pagamento
+                                                </NavLink>
+                                                <NavLink to='/usuarios' className={({ isActive }) => `
+                                                opcaoMenu btnTrocarDePaginaMenu
+                                                ${isActive ? 'opcaoMenuSelecionado' : ''}
+                                            ` } data-pagina="usuarios">
+                                                    <i className="material-icons">group</i>
+                                                    Usuários
+                                                </NavLink>
+                                                <NavLink to='/configuracoes' className={({ isActive }) => `
+                                                opcaoMenu btnTrocarDePaginaMenu
+                                                ${isActive ? 'opcaoMenuSelecionado' : ''}
+                                            ` } data-pagina="formularioConfigSistema">
+                                                    <i className="material-icons">settings</i>
+                                                    Sistema
+                                                </NavLink>
+                                            </div>
 
-            </section>
+                                        </section>
 
-            <section id="main">
-                <header id="header">
-                    <p id="textoBoasVindas">Olá {LoginService.usuarioLogado?.nome}</p>
-                    <button id="btnAbrirMenuUsuario" className="material-icons">person</button>
-                </header>
-    
-                <Outlet />
-            </section>
-            
-        </> :
+                                        <section id="main">
+                                            <header id="header">
+                                                <p id="textoBoasVindas">Olá {LoginService.usuarioLogado?.nome}</p>
+                                                <button id="btnAbrirMenuUsuario" className="material-icons">person</button>
+                                            </header>
 
-        <div id="divLoader">
-            <ThreeDots
-                color="#272D36" // Cor do spinner
-                height={100} // Altura do spinner
-                width={100} // Largura do spinner
-            />
-        </div>
-        
+                                            <Outlet />
+                                        </section>
+                                    </TicketsProvider>
+                                </MensalidadesProvider>
+                            </MensalistasProvider>
+                        </FormasDePagamentoProvider>
+                    </PrecificacoesProvider>
+                </SistemaProvider>
+            </UsuariosProvider>
+
+            :
+
+            <div id="divLoader">
+                <ThreeDots
+                    color="#272D36" // Cor do spinner
+                    height={100} // Altura do spinner
+                    width={100} // Largura do spinner
+                />
+            </div>
+
 
     )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -16,66 +16,45 @@ import CadastrarFormaDePagamento from './pages/CadastrarFormaDePagamento';
 import Usuarios from './pages/Usuarios';
 import CadastrarUsuario from './pages/CadastrarUsuario';
 import ConfiguracoesDoSistema from './pages/ConfiguracoesDoSistema';
-import PrecificacoesProvider from './contexts/PrecificacaoContext';
-import TicketsProvider from './contexts/TicketContext';
 import EditarTicket from './pages/EditarTicket';
-import FormasDePagamentoProvider from './contexts/FormaDePagamentoContext';
 import VisualizarTicket from './pages/VisualizarTicket';
-import MensalistasProvider from './contexts/MensalistasContext';
 import EditarMensalista from './pages/EditarMensalista';
-import MensalidadesProvider from './contexts/MensalidadesContext';
 import EditarPrecificacao from './pages/EditarPrecificacao';
 import EditarFormaDePagamento from './pages/EditarFormaDePagamento';
-import UsuariosProvider from './contexts/UsuariosContext';
 import EditarUsuario from './pages/EditarUsuario';
-import SistemaProvider from './contexts/SistemaContext';
 import TelaLogin from './pages/TelaLogin';
 
 
 function AppRoutes() {
   return (
-    <UsuariosProvider>
-      <SistemaProvider>
-        <PrecificacoesProvider>
-          <FormasDePagamentoProvider>
-            <MensalistasProvider>
-              <MensalidadesProvider>
-                <TicketsProvider>
-                  <BrowserRouter>
-                    <Routes>
-                      <Route path='/' element={<PaginaBase />}>
-                        <Route index element={<Home />} />
-                        <Route path='estacionamento' element={<Estacionamento />} />
-                        <Route path='estacionamento/cadastrarTicket' element={<CadastrarTicket />} />
-                        <Route path='estacionamento/editarTicket/:id' element={<EditarTicket />} />
-                        <Route path='estacionamento/visualizarTicket/:id' element={<VisualizarTicket />} />
-                        <Route path='mensalistas' element={<Mensalistas />} />
-                        <Route path='mensalistas/cadastrarMensalista' element={<CadastrarMensalista />} />
-                        <Route path='mensalistas/editarMensalista/:id' element={<EditarMensalista />} />
-                        <Route path='mensalidades' element={<Mensalidades />} />
-                        <Route path='mensalidades/cadastrarMensalidade' element={<CadastrarMensalidade />} />
-                        <Route path='precificacoes' element={<Precificacoes />} />
-                        <Route path='precificacoes/cadastrarPrecificacao' element={<CadastrarPrecificacao />} />
-                        <Route path='precificacoes/editarPrecificacao/:id' element={<EditarPrecificacao />} />
-                        <Route path='formasDePagamento' element={<FormasDePagamento />} />
-                        <Route path='formasDePagamento/cadastrarFormaDePagamento' element={<CadastrarFormaDePagamento />} />
-                        <Route path='formasDePagamento/editarFormaDePagamento/:id' element={<EditarFormaDePagamento />} />
-                        <Route path='usuarios' element={<Usuarios />} />
-                        <Route path='usuarios/cadastrarUsuario' element={<CadastrarUsuario />} />
-                        <Route path='usuarios/editarUsuario/:id' element={<EditarUsuario />} />
-                        <Route path='configuracoes' element={<ConfiguracoesDoSistema />} />
-                      </Route>
-                      <Route path='login' element={<TelaLogin />} />
-                    </Routes>
-                  </BrowserRouter>
-                </TicketsProvider>
-              </MensalidadesProvider>
-            </MensalistasProvider>
-          </FormasDePagamentoProvider>
-        </PrecificacoesProvider>
-      </SistemaProvider>
-    </UsuariosProvider>
 
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<PaginaBase />}>
+          <Route index element={<Home />} />
+          <Route path='estacionamento' element={<Estacionamento />} />
+          <Route path='estacionamento/cadastrarTicket' element={<CadastrarTicket />} />
+          <Route path='estacionamento/editarTicket/:id' element={<EditarTicket />} />
+          <Route path='estacionamento/visualizarTicket/:id' element={<VisualizarTicket />} />
+          <Route path='mensalistas' element={<Mensalistas />} />
+          <Route path='mensalistas/cadastrarMensalista' element={<CadastrarMensalista />} />
+          <Route path='mensalistas/editarMensalista/:id' element={<EditarMensalista />} />
+          <Route path='mensalidades' element={<Mensalidades />} />
+          <Route path='mensalidades/cadastrarMensalidade' element={<CadastrarMensalidade />} />
+          <Route path='precificacoes' element={<Precificacoes />} />
+          <Route path='precificacoes/cadastrarPrecificacao' element={<CadastrarPrecificacao />} />
+          <Route path='precificacoes/editarPrecificacao/:id' element={<EditarPrecificacao />} />
+          <Route path='formasDePagamento' element={<FormasDePagamento />} />
+          <Route path='formasDePagamento/cadastrarFormaDePagamento' element={<CadastrarFormaDePagamento />} />
+          <Route path='formasDePagamento/editarFormaDePagamento/:id' element={<EditarFormaDePagamento />} />
+          <Route path='usuarios' element={<Usuarios />} />
+          <Route path='usuarios/cadastrarUsuario' element={<CadastrarUsuario />} />
+          <Route path='usuarios/editarUsuario/:id' element={<EditarUsuario />} />
+          <Route path='configuracoes' element={<ConfiguracoesDoSistema />} />
+        </Route>
+        <Route path='login' element={<TelaLogin />} />
+      </Routes>
+    </BrowserRouter>
 
   );
 }
